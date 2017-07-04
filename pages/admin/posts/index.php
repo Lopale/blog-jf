a8378ae141d54f005f647d630de2522a53822d94
Gnia <grillon.d@gmail.com>
2017-07-04 17:34:38 +0200

    Liste des Posts (Articles / Chapitre) dans l'admin

--- pages/admin/posts/index.php
+++ pages/admin/posts/index.php
@@ -1,1 +1,30 @@
+<?php
+
+$datas = App::getInstance()->getTable ('Post')->last();
+
+?>
+
 <h1>ADMIN DU SITE</h1>
+
+
+<table class="table">
+
+	<thead>
+		<tr>
+			<td>ID</td>
+			<td>Titre</td>
+			<td>Actions</td>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($datas as $post): ?>
+			<tr>
+				<td><?= $post->id_article; ?></td>
+				<td><?= $post->titre_article; ?></td>
+				<td>
+					<a class="btn btn-primary" href="?posts.edit&id=<?= $post->id_article; ?>">Editer</a>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
